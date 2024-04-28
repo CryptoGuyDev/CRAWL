7672969b38f065b58a5f15a5a814c69da3956153
CryptoGuy.Dev <151238210+CryptoGuyDev@users.noreply.github.com>
2024-04-28 13:38:00 +0500

    Update MeetingCard.tsx

--- components/MeetingCard.tsx
+++ components/MeetingCard.tsx
@@ -33,6 +33,7 @@ const MeetingCard = ({
   return (
     <section className="flex min-h-[258px] w-full flex-col justify-between rounded-[14px] bg-dark-1 px-5 py-8 xl:max-w-[568px]">
       <article className="flex flex-col gap-5">
+        <Image src={icon} alt="upcoming" width={28} height={28} />
         <div className="flex justify-between">
           <div className="flex flex-col gap-2">
             <h1 className="text-2xl font-bold">{title}</h1>
@@ -43,15 +44,6 @@ const MeetingCard = ({
       <article className={cn("flex justify-center relative", {})}>
         <div className="relative flex w-full max-sm:hidden">
           {avatarImages.map((img, index) => (
-            <Image
-              key={index}
-              src={img}
-              alt="attendees"
-              width={40}
-              height={40}
-              className={cn("rounded-full", { absolute: index > 0 })}
-              style={{ top: 0, left: index * 28 }}
-            />
           ))}
           <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
             +5
