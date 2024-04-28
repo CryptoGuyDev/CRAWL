6a3ced189bc4d16bf04fb2f4c8cd948d6cafa289
CryptoGuy.Dev <151238210+CryptoGuyDev@users.noreply.github.com>
2024-04-28 13:38:55 +0500

    Update MeetingCard.tsx

--- components/MeetingCard.tsx
+++ components/MeetingCard.tsx
@@ -44,6 +44,15 @@ const MeetingCard = ({
       <article className={cn("flex justify-center relative", {})}>
         <div className="relative flex w-full max-sm:hidden">
           {avatarImages.map((img, index) => (
+          <Image
+              key={index}
+              src={img}
+              alt="attendees"
+              width={40}
+              height={40}
+              className={cn("rounded-full", { absolute: index > 0 })}
+              style={{ top: 0, left: index * 28 }}
+            />
           ))}
           <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
             +5
@@ -66,12 +75,7 @@ const MeetingCard = ({
               }}
               className="bg-dark-4 px-6"
             >
-              <Image
-                src="/icons/copy.svg"
-                alt="feature"
-                width={20}
-                height={20}
-              />
+  
               &nbsp; Copy Link
             </Button>
           </div>
